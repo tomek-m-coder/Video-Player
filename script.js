1d39ae2988d8c4d00914cd8442fd3ba531f2b06c
tomek-m-coder <60541005+tomek-m-coder@users.noreply.github.com>
2024-08-11 18:05:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-
-
-// Play & Pause ----------------------------------- //
-
-
-
-// Progress Bar ---------------------------------- //
-
-
-
-// Volume Controls --------------------------- //
-
-
-
-// Change Playback Speed -------------------- //
-
-
-
-// Fullscreen ------------------------------- //
-
-
+// Function to quickly assign element constants -------------------------- //
+function getEl(el) {
+    return document.querySelector(el);
+  }
+  
+  // Play & Pause ---------------------------------------------------------- //
+  function showPlayIcon() {
+    elements.playBtn.classList.replace('fa-pause', 'fa-play');
+    elements.playBtn.setAttribute('title', 'Play');
+  }
+  
